eaa438e8ca4e583fa41c7bc27fc7e18f28ccfaa8
david-Macas-12333 <macasdavid46@gmai.com>
2025-11-28 09:04:35 -0500

    Agregamos la clases Controlador_de_SignosVitales

---
+++ src/main/java/edu/unl/cc/proyecto_programacion/Controlador/Controlador_de_SignosVitales.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.unl.cc.proyecto_programacion.Controlador;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class Controlador_de_SignosVitales {
+    
+    
+    
+}
