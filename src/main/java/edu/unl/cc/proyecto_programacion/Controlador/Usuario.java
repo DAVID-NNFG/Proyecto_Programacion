cbfef520217c34c80417dcc802989a112d8cad9d
DAVID-NNFG <macasdavidvictor123@gmai.com>
2025-11-28 08:59:45 -0500

    Agregamos la clases Usuario

---
+++ src/main/java/edu/unl/cc/proyecto_programacion/Controlador/Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.unl.cc.proyecto_programacion.Controlador;
+
+/**
+ *
+ * @author DAVID-NNFG
+ */
+public class Usuario {
+    
+    private String rol; 
+    private String nombre;
+
+    public Usuario(String rol, String nombre) {
+        this.rol = rol;
+        this.nombre = nombre;
+    }
+    
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "rol=" + rol + ", nombre=" + nombre + '}';
+    }
+    
+    
+    
+}
